3032e275594707123f994b909cdbd15d4ee47f52
Dinush Chathurya <dinushchathurya21@gmail.com>
2022-11-13 10:40:59 +0530

    :hammer: Refactor entrypoint.sh with exception handling

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,18 @@
 #!/bin/sh
 
-docker login -u $1 -p $2 &&
-docker build $3 --file $4 --tag $5/$6:$7 &&
-docker push $5/$6:$7
+if [[ $1 != "" && $2 != "" ]] 
+then
+    docker login -u $1 -p $2
+
+    if [[ $3 != "" && $4 != "" && $5 != "" && $6 != "" && $7 != "" ]]
+    then
+        docker build $3 --file $4 --tag $5/$6:$7 &&
+        docker push $5/$6:$7
+    else
+        echo "Missing arguments, skipping..."
+        exit 1
+    fi
+else
+  echo "No credentials provided, skipping..."
+  exit 1
+fi
